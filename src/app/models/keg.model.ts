233bdacd3975f5c05290d0658943aac0750bc15d
Charley McGowan and Robert Lee <charley@>
2019-01-23 13:31:40 -0800

    Add a progress bar to beer level.

--- src/app/models/keg.model.ts
+++ src/app/models/keg.model.ts
@@ -1,11 +1,16 @@
 class Keg {
-  public poursRemaining = 124;
+  public poursRemaining : number  = 124;
+  public percentBeerRemaining : number = 0;
   constructor(public beerBrand: string, public beerName: string , public price: number, public alcByVolume: number){
 
   }
 
   pourABeer(){
     this.poursRemaining--;
+  }
+
+  percentRemaining(){     
+    return this.poursRemaining/124*100;
   }
 }
 
